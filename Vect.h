1969b7e2480d906862a4d599a5e85fd787c6f038
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2014-08-23 11:50:37 +0200

    example of load and store

--- Vect.h
+++ Vect.h
@@ -7,7 +7,22 @@
 
 #include "VecFun.h"
 
+template<typename T>
+extvec::Vec4D<T> load3(Vector3D<T> const & v) {
+  using VA = typename extvec::ExtVecTraits<T,4>::typeA;
+  return *(VA const *)(&v.x());
+}
 
+template<typename V >
+auto store3(V ev) -> Vector3D<typename std::remove_const<typename std::remove_reference<decltype(ev[0])>::type>::type> {
+  using T = typename std::remove_const<typename std::remove_reference<decltype(ev[0])>::type>::type;
+  using VA = typename extvec::ExtVecTraits<T,4>::typeA;
+  extvec::IVec4D<T> mask = {0,1,2,7};
+  Vector3D<T> v; 
+  decltype(auto) res = *(VA*)(&v.x());
+  res = __builtin_shuffle(ev,res,mask);
+  return v;
+}
 
 
 namespace vect3d {
--- slp.cc
+++ slp.cc
@@ -1,12 +1,23 @@
-#include "Vector3D.h"
+#include "Vect.h"
 
+#ifndef USEVECEXT
 using Vec3D = Vector3D<float>;
-//using Vec3D = Vec4D<float>;
-
+#else
+using Vec3D = extvec::Vec4D<float>;
+#endif
 
 
 Vec3D triad(Vec3D const & a, Vec3D const & b, Vec3D const & c) {
   return a+b*c;
 }
 
+Vec3D triadV(Vec3D a, Vec3D b, Vec3D c) {
+  return a+b*c;
+}
 
+
+#ifndef USEVECEXT
+Vec3D triad3(Vec3D const & a, Vec3D const & b, Vec3D const & c) {
+  return store3(load3(a)+load3(b)*load3(c));
+}
+#endif
